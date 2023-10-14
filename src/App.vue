3bea4532cbc5c8bf9f2233ec8f398780c0c71b5b
Sourav Ghosh @CrowntailTech <sourav.ghosh@crowntailtech.com>
2023-10-14 13:13:33 +0530

    project setup

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="mt-6 text-center text-6xl">hello world</h1>
+</template>
+;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+/*global module, require*/
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+};
